e458a8b5e8141e7a24274e5865290de27b15b557
tantradnya <neeraj.adsul@gmail.com>
2021-04-10 08:41:31 +0100

    rename for meeting assignment rubrik

---
+++ main.py
@@ -0,0 +1,20 @@
+from QuoteEngine import CSVIngestor, DocxIngestor, PDFIngestor, TXTIngestor
+from QuoteEngine import Ingestor
+from MemeEngine import MemeEngine
+
+if __name__ == '__main__':
+    base_path = '/Users/Neeraj/Development/meme_generator/_data/DogQuotes/'
+    # print(CSVIngestor.parse(base_path + 'DogQuotesCSV.csv'))
+    # print(DocxIngestor.parse(base_path + 'DogQuotesDOCX.docx'))
+    # print(PDFIngestor.parse(base_path + 'DogQuotesPDF.pdf'))
+    # print(TXTIngestor.parse(base_path + 'DogQuotesTXT.txt'))    
+    # print(Ingestor.Ingestor.parse(base_path + 'DogQuotesCSV.csv'))
+
+    meme_gen = MemeEngine('/Users/Neeraj/Development/meme_generator/_data')
+    output = meme_gen.make_meme(
+       '/Users/Neeraj/Development/meme_generator/_data/photos/dog/xander_1.jpg',
+       'Hello World!',
+       'Macbook Pro'
+    )
+    print(output)
+    
