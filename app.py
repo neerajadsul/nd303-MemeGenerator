87ac86427c876c0066730256a31bffe66a9f213a
tantradnya <neeraj.adsul@gmail.com>
2021-04-10 11:44:42 +0100

    completed TODO in

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import random
 import os
 import requests
 from flask import Flask, render_template, abort, request
+from urllib.parse import urlparse
 
 from QuoteEngine import Ingestor
 from MemeEngine import MemeEngine
@@ -28,6 +29,10 @@ def setup():
     imgs = None
     for root, dirs, files in os.walk(images_path):
         imgs = [os.path.join(root, name) for name in files]
+
+    static_path = './static'
+    if not os.path.exists(static_path):
+        os.mkdir(static_path)
 
     return quotes, imgs
 
@@ -61,8 +66,24 @@ def meme_post():
     # 2. Use the meme object to generate a meme using this temp
     #    file and the body and author form paramaters.
     # 3. Remove the temporary saved image.
+    if request.method == 'POST':
+        img_url = request.form.get('image_url', default=None)
+        body = request.form.get('body', default=None)
+        author = request.form.get('author', default=None)
 
-    path = None
+        if img_url is not None:
+            response = requests.get(img_url)
+            temp_file = os.path.basename(urlparse(img_url).path)
+            temp_file = os.path.join('./tmp', temp_file)
+            print(temp_file)
+            with open(temp_file, 'wb') as fp:
+                fp.write(response.content)
+        
+        meme = MemeEngine('./static')
+        path = meme.make_meme(temp_file, body, author)
+
+        os.remove(temp_file)
+        
 
     return render_template('meme.html', path=path)
 
