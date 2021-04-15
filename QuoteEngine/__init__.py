f74b00d301639f5f524f88d45ec8549aa05db620
tantradnya <neeraj.adsul@gmail.com>
2021-04-15 13:54:34 +0100

    programatically check and create required dirs

--- QuoteEngine/__init__.py
+++ QuoteEngine/__init__.py
@@ -6,3 +6,14 @@ from .DocxIngestor import DocxIngestor
 from .PDFIngestor import PDFIngestor
 from .TXTIngestor import TXTIngestor
 from .QuoteModel import QuoteModel
+
+import os
+
+TEMP_PATH = './tmp'
+STATIC_PATH = './static'
+
+if not os.path.exists(TEMP_PATH):
+    os.mkdir(TEMP_PATH)
+
+if not os.path.exists(STATIC_PATH):
+    os.mkdir(STATIC_PATH)
